f6aa2e96e5f8992f1a26e72c7f9012db6de8ecf2
nana.ginzbourg@gmail.com <nana.ginzbourg@gmail.com@a4ab9e1f-4ca4-5595-3473-816f58cd02b9>
2011-09-13 21:00:29 +0000

    - switch Pareto to run on TCP and not UDP - add correct configuration parameter - add an option to run the simulation with  1 traffic type only

--- main.tcl
+++ main.tcl
@@ -7,7 +7,6 @@ source utilities.tcl
 # in case commande line parameters were provided, override default values from configuration.tcl file
 foreach { key value } $argv {
 	switch -- $key {
-		-tpFile  { set cfg_(TOPOLOGY_FILE) $value }
 		-nn   { set cfg_(NUM_OF_CLIENTS) $value }
 		-topX   { set cfg_(TOPOLOGY_X) $value }
 		-topY   { set cfg_(TOPOLOGY_Y) $value }
@@ -15,7 +14,10 @@ foreach { key value } $argv {
 		-dl  { set cfg_(DROP_TAIL_DELAY) $value }
 		-qs   { set cfg_(QUEUE_SIZE) $value }
 		-cache { set cfg_(CACHE) $value }
-
+		-webUsers { set cfg_(WEB) $value }
+		-videoUsers { set cfg_(VIDEO) $value } 
+		-filesUsers { set cfg_(FILES) $value } 
+		-voipUsers { set cfg_(VOIP) $value }  
 	}
 }
 
@@ -29,7 +31,7 @@ set ns [new Simulator]
 set f [open out.tr w]
 $ns trace-all $f
 set httpLog [open "http.log" w]
-#add-packet-header IP UDP;
+#add-packet-header IP TCP;
 #Trace set show_tcphdr_ 1;
 
 #Open the nam trace file 
@@ -60,10 +62,8 @@ set startIndex 0
 set first 0
 set last [expr {$first+$WEB_USERS}]
 for { set i $first} { $i<$last } {incr i} {
-   puts "web: $i"	
-   set paretoWebServer($i) [createParetoFlow $ns $server $UE($i) 210 510ms 500ms 0.5mb 1.1 1 $i]
-   set paretoWebClient($i) [createParetoFlow $ns $UE($i) $server 210 510ms 500ms 0.5mb 1.1 1 $i]
-   $ns at 0.0 "$paretoWebServer($i) start" 
+   puts "web: $i"
+   set paretoWebClient($i) [createParetoFlow $ns $UE($i) $server $cfg_(WEB_BURST_TIME) $cfg_(WEB_PACKET_SIZE) $cfg_(WEB_IDLE_TIME) $cfg_(WEB_RATE) $cfg_(WEB_SHAPE) 1 $i]  
    $ns at 0.0 "$paretoWebClient($i) start"			
 }
 # define video users
@@ -71,9 +71,7 @@ set first $last
 set last [expr {$last+$VIDEO_USERS}]
 for { set i $first} { $i<$last } {incr i} {
    puts "videp: $i"	
-   set paretoVideoServer($i) [createParetoFlow $ns $server $UE($i) 210 1000ms 500ms 0.5mb 1.1 2 $i]
-   set paretoVideoClient($i) [createParetoFlow $ns $UE($i) $server 210 1000ms 500ms 0.5mb 1.1 2 $i]
-   $ns at 0.0 "$paretoVideoServer($i) start" 
+   set paretoVideoClient($i) [createParetoFlow $ns $UE($i) $server $cfg_(VIDEO_BURST_TIME) $cfg_(VIDEO_PACKET_SIZE) $cfg_(VIDEO_IDLE_TIME) $cfg_(VIDEO_RATE) $cfg_(VIDEO_SHAPE) 2 $i]
    $ns at 0.0 "$paretoVideoClient($i) start"	 			
 }
 # define files users
@@ -81,9 +79,7 @@ set first $last
 set last [expr {$last+$FILES_USERS}]
 for { set i $first} { $i<$last } {incr i} {
     puts "files: $i"	
-    set paretoFilesServer($i) [createParetoFlow $ns $server $UE($i) 210 500ms 500ms 0.5mb 1.1 3 $i]
-    set paretoFilesClient($i) [createParetoFlow $ns $UE($i) $server 210 500ms 500ms 0.5mb 1.1 3 $i]
-    $ns at 0.0 "$paretoFilesServer($i) start"
+    set paretoFilesClient($i) [createParetoFlow $ns $UE($i) $server $cfg_(FILES_BURST_TIME) $cfg_(FILES_PACKET_SIZE) $cfg_(FILES_IDLE_TIME) $cfg_(FILES_RATE) $cfg_(FILES_SHAPE) 3 $i]
     $ns at 0.0 "$paretoFilesClient($i) start" 				 				
 }
 # define voip users
@@ -91,15 +87,13 @@ set first $last
 set last [expr {$last+$VOIP_USERS}]
 for { set i $first} { $i<$last } {incr i} {
     puts "voip: $i"
-   set cbrVoipServer($i) [createCbrFlow $ns $server $UE($i) 210 0.25mb 5 $i]
-   set cbrVoipClient($i) [createCbrFlow $ns $UE($i) $server 210 0.25mb 5 $i]
-   $ns at 0.0 "$cbrVoipServer($i) start"
+   set cbrVoipClient($i) [createCbrFlow $ns $UE($i) $server $cfg_(VOIP_PACKET_SIZE) $cfg_(VOIP_MAX_PACKETS)  $cfg_(VOIP_RATE) $cfg_(VOIP_INTERVAL) 5 $i]
    $ns at 0.0 "$cbrVoipClient($i) start" 			 			
 }
 
 
 #Finish the simulation at 3.0 sec
-$ns at 3.0 "finish"
+$ns at 10.0 "finish"
 
 proc finish {} {
 	global ns f nf
